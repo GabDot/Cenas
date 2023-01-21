d8b0144a0c5a1b3d38c12ecfdd521cb071e50598
GabDot <gabsilvatex@gmail.com>
2023-01-21 01:10:11 +0000

    navbar

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './screens/home';
 import User from './screens/user';
 import * as SplashScreen from "expo-splash-screen"
 import { useEffect } from 'react';
 import { useFonts } from 'expo-font'
 export default function App() {
-  const Tab = createMaterialBottomTabNavigator();
+  const Tab = createBottomTabNavigator();
   const [fontsLoaded] = useFonts({
     'sans-bold': require('./assets/fonts/PublicSans-Bold.ttf'),
     'sans-medium': require('./assets/fonts/PublicSans-Medium.ttf'),
@@ -33,41 +34,58 @@ export default function App() {
   }
 
   return (
+    <>
+    <StatusBar translucent></StatusBar>
+    
 
     <NavigationContainer>
       <Tab.Navigator
-        barStyle={[styles.navbar,{ backgroundColor: 'white' }]}>
-        <Tab.Screen name="Home" component={Home}
+        
+        barStyle={[styles.navbar,{ backgroundColor: 'white' }]}
+        screenOptions={{
+          tabBarActiveTintColor: '#1E1E1E',
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: { height: 80 },
+        }}>
+        <Tab.Screen name="Holele" component={Home}
           options={{
-            tabBarLabel: 'Hoho',
+            
             tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={color} size={26} />
+              <MaterialCommunityIcons name="home" color={color} size={38} />
             ),
           }} />
         <Tab.Screen name="User" component={User}
           options={{
-            tabBarLabel: 'Eu',
+            
             tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="account-circle" color={color} size={26} />
+              <MaterialCommunityIcons name="clipboard-list" color={color} size={38} />
             ),
           }} />
           <Tab.Screen name="Notas" component={User}
           options={{
-            tabBarLabel: 'Notas',
+            
             tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="account-circle" color={color} size={26} />
+              <MaterialCommunityIcons name="account-circle" color={color} size={38} />
             ),
           }} />
           <Tab.Screen name="Ementa" component={User}
           options={{
-            tabBarLabel: 'Ementa',
+            
             tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="account-circle" color={color} size={26} />
+              <MaterialCommunityIcons name="silverware" color={color} size={38} />
+            ),
+          }} />
+          <Tab.Screen name="Mais" component={User}
+          options={{
+            
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="dots-horizontal" color={color} size={38} />
             ),
           }} />
       </Tab.Navigator>
     </NavigationContainer>
-
+    </>
   );
 }
 
