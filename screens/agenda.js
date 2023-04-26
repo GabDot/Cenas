39d7677bd5d6e267bb2d3f202e65845e7486b0c5
GabDot <gabsilvatex@gmail.com>
2023-04-26 01:06:59 +0100

    agenda v2

--- screens/agenda.js
+++ screens/agenda.js
@@ -1,62 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, Modal, TouchableOpacity,StyleSheet } from 'react-native';
-import { Calendar,LocaleConfig } from 'react-native-calendars';
+import React, { useState, useEffect,useCallback,useMemo } from 'react';
+import { Text, View, Modal, TouchableOpacity,StyleSheet,ScrollView } from 'react-native';
+import { Calendar,LocaleConfig,CalendarList } from 'react-native-calendars';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Header from '../components/Header';
 import { global } from "../styles/globals";
 import { database } from '../firebase';
+import { auth } from '../firebase';
 
-const AgendaScreen = () => {
+const AgendaScreen = React.memo(() => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const uid = auth.currentUser.uid
+  const agendaRef = database.collection('users').doc(uid).collection('agenda');
   const [eventDates, setEventDates] = useState([]);
   const eventosRef = database.collection('eventos');
-  const [eventos, setEventos] = useState([]);
-  
+  const [agenda, setAgenda] = useState([]);
+  const today = new Date().toISOString().split('T')[0]; // get today's date in ISO format
+  function formatDate(timestamp) {
+    const date = new Date(timestamp.seconds * 1000);
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');     
+    return `${day}/${month}/${year}`;
+  }
+const noEventsMessage = selectedDate === today
+  ? 'Não tens nada agendado para hoje'
+  : 'Não tens nada agendado para este dia';
   async function loadDataFromStorage() {
     const agendaData = await AsyncStorage.getItem('agenda');
-  
+    
     if (agendaData !== null) {
       const parsedData = JSON.parse(agendaData).map((event) => {
         const [day, month, year] = event.data.split('/');
-        const date = new Date(year, month - 1, day);
+        const localDate = new Date(year, month - 1, day,0,0,0);
+        const offset = localDate.getTimezoneOffset() * 60000; // convert minutes to milliseconds
+        const utcDate = new Date(localDate.getTime() - offset);
         return {
           ...event,
-          data: date.toISOString().split('T')[0]
+          data: utcDate.toISOString().split('T')[0]
         };
       });
       setEventDates(parsedData);
+     
       
     }
   }
+  
   useEffect(() => {
     loadDataFromStorage();
+    
     const today = new Date();
     setSelectedDate(today.toISOString().split('T')[0]);
-    eventosRef.onSnapshot((querySnapshot) => {
-      const eventos = [];
-      querySnapshot.forEach((documentSnapshot) => {
-          eventos.push(documentSnapshot.data());
-        });
-  
-        if (eventos.length > 0) {
-          setEventos(eventos);
-          AsyncStorage.setItem('eventos', JSON.stringify(eventos));
-        }
+    agendaRef.onSnapshot({ source: 'server' },(querySnapshot) => {
+      const agenda = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data().data;
+        const titulo = doc.data().titulo;
+        agenda.push({ data: formatDate(data), titulo });
       });
+      if (agenda.length > 0) {
+        setAgenda(agenda);
+        AsyncStorage.setItem('agenda', JSON.stringify(agenda));
+        loadDataFromStorage();
+        
+      }
+    });
   }, []);
 
-  
+  useEffect(() => {
+    console.log(eventDates);
+  }, [eventDates]);
 
-  const onDayPress = (day) => {
+  const onDayPress = useCallback((day) => {
     setSelectedDate(day.dateString);
     setModalVisible(true);
-  };
+  }, []);
 
-  const closeModal = () => {
-    setSelectedDate(null);
-    setModalVisible(false);
-  };
+ 
   LocaleConfig.locales['pt'] = {
     monthNames: [
       'Janeiro',
@@ -77,49 +97,60 @@ const AgendaScreen = () => {
     today: 'Hoje',
     
   };
+  
 LocaleConfig.defaultLocale = 'pt'
+const markedDates = useMemo(() => {
+  return eventDates.reduce((obj, date) => {
+    obj[date.data] = {
+      selected: true,
+      title: date.titulo,
+    };
+    return obj;
+  }, {});
+}, [eventDates]);
   return (
     <>
     <Header></Header>
     <View style={{ flex: 1 }}>
-      <Calendar
-        onDayPress={onDayPress}
-        markedDates={eventDates.reduce((obj, date) => {
-          obj[date.data] = {
-            selected: true,
-            title: date.titulo,
-          };
-          return obj;},{})}
-        pagingEnabled={true}
-        hideExtraDays={true}
-        firstDay={1}
-        theme={{
-          backgroundColor:'rgb(242,242,242)',
-          calendarBackground:'rgb(242,242,242)',
-          textMonthFontFamily:'sans-bold',
-          textMonthFontSize:36,
-          monthTextColor:'#1E1E1E',
-
-          
-        }}
-      />
+    <CalendarList
+  onDayPress={onDayPress}
+  markedDates={markedDates}
+  hideArrows
+  pastScrollRange={10}
+  futureScrollRange={10}
+  pagingEnabled={true}
+  hideExtraDays={true}
+  firstDay={1}
+  horizontal={true}
+  theme={{
+    backgroundColor: 'rgb(242,242,242)',
+    calendarBackground: 'rgb(242,242,242)',
+    textMonthFontFamily: 'sans-bold',
+    textMonthFontSize: 36,
+    monthTextColor: '#1E1E1E',
+  }}
+/>
       
-       <View style={styles.infoContainer}>
- 
-    <Text style={[global.p]}>
-      {eventDates
-        .filter(event => event.data === selectedDate)
-        .map((event, index) => `${event.titulo}\n\n`)
-        .join('')
-      }
-    </Text>
-  
-</View>
+      <View style={styles.infoContainer}>
+    {eventDates.filter(event => event.data === selectedDate).length > 0 ? (
+      <Text style={[global.p]}>
+        {eventDates
+          .filter(event => event.data === selectedDate)
+          .map((event, index, array) => {
+            const separator = index === array.length - 1 ? '' : '\n\n';
+            return `${event.titulo}${separator}`;
+          })
+        }
+      </Text>
+    ) : (
+      <Text style={[global.p]}>{noEventsMessage}</Text>
+    )}
+  </View>
       
     </View>
     </>
   );
-};
+});
 const styles = StyleSheet.create({
   infoContainer:{
     height:'auto',
@@ -132,4 +163,4 @@ const styles = StyleSheet.create({
   },
   
 });
-export default AgendaScreen;
+export default AgendaScreen;  
