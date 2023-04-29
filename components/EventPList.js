09120c4d63f482727981b3857373b24a0c96833b
HuGo <gabsilvatex@gmail.com>
2023-04-29 13:42:24 +0100

    agenda completa sem nots

--- components/EventPList.js
+++ components/EventPList.js
@@ -1,32 +1,55 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity, Modal, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { global } from '../styles/globals';
 import { database } from '../firebase';
 import { auth } from '../firebase';
 
 const EventList = ({ events, noEventsMessage, selectedDate }) => {
   const uid = auth.currentUser.uid
-const agendaPRef = database.collection('users').doc(uid).collection('agendaP')
+  const agendaPRef = database.collection('users').doc(uid).collection('agendaP')
   const [modalVisible, setModalVisible] = useState(false);
   const [modal2Visible, setModal2Visible] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState(['a']);
+
   const [newText, setNewText] = useState('')
-  
+
   const filteredEvents = events.filter(event => event.data === selectedDate);
 
-  const handleEdit = (id) => {
- 
+  const handleEdit = async (id) => {
+    setModal2Visible(false);
     agendaPRef.doc(id).update({
       titulo: newText
     })
-    setModal2Visible(false);
-    
+
+    const item = await AsyncStorage.getItem('agendaP');
+
+
+    const parsedItem = JSON.parse(item);
+
+
+    parsedItem.selectedEvent = newText
+
+
+    await AsyncStorage.setItem('myItem', JSON.stringify(parsedItem));
+
+
   };
 
-  const handleDelete = (id) => {
-    
-    agendaPRef.doc(id).delete()
+  const handleDelete = async (id) => {
     setModalVisible(false);
+    agendaPRef.doc(id).delete()
+    const item = await AsyncStorage.getItem('agendaP');
+
+
+    const parsedItem = JSON.parse(item);
+
+
+    delete parsedItem.selectedEvent;
+
+
+    await AsyncStorage.setItem('myItem', JSON.stringify(parsedItem));
+
   };
 
   return (
@@ -39,25 +62,25 @@ const agendaPRef = database.collection('users').doc(uid).collection('agendaP')
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={[{fontWeight:'bold'},global.h2]}>{selectedEvent.titulo }</Text>
-            <Text style={[{marginTop:10,marginBottom:20},global.p]}>O que pretende fazer com este evento?</Text>
+            <Text style={[{ fontWeight: 'bold' }, global.h2]}>{selectedEvent.titulo}</Text>
+            <Text style={[{ marginTop: 10, marginBottom: 20 }, global.p]}>O que pretende fazer com este evento?</Text>
             <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-            <TouchableOpacity style={[styles.editButton]}onPress={() => {
-  setModal2Visible(true) 
-  setModalVisible(false);
-  
-}} >
-    <Text style={[global.p,{color:'white'}]}>Edit</Text>
-  </TouchableOpacity>
-  <TouchableOpacity style={[styles.deleteButton]} onPress={() => handleDelete(selectedEvent.id)}>
-    
-    <Text style={[global.p,{color:'white'}]}>Delete</Text>
-  </TouchableOpacity>
-             
+              <TouchableOpacity style={[styles.editButton]} onPress={() => {
+                setModal2Visible(true)
+                setModalVisible(false);
+
+              }} >
+                <Text style={[global.p, { color: 'white' }]}>Edit</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.deleteButton]} onPress={() => handleDelete(selectedEvent.id)}>
+
+                <Text style={[global.p, { color: 'white' }]}>Delete</Text>
+              </TouchableOpacity>
+
             </View>
             <TouchableOpacity style={[styles.closeButton]} onPress={() => setModalVisible(false)}>
-    <Text style={global.p}>X</Text>
-  </TouchableOpacity>
+              <Text style={global.p}>X</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </Modal>
@@ -69,27 +92,27 @@ const agendaPRef = database.collection('users').doc(uid).collection('agendaP')
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={[{fontWeight:'bold'},global.h2]}>{selectedEvent.titulo }</Text>
+            <Text style={[{ fontWeight: 'bold' }, global.h2]}>{selectedEvent.titulo}</Text>
             <TextInput
-      style={styles.input}
-      placeholder={selectedEvent.titulo}
-      onChangeText={text => setNewText(text)}
-      value={newText}
-    />
+              style={styles.input}
+              placeholder={selectedEvent.titulo}
+              onChangeText={text => setNewText(text)}
+              value={newText}
+            />
             <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-            <TouchableOpacity style={[styles.deleteButton]} onPress={() => handleDelete(selectedEvent.id)}>
-    
-    <Text style={[global.p,{color:'white'}]}>Cancel</Text>
-  </TouchableOpacity>
-            <TouchableOpacity style={[styles.editButton]} >
-    <Text style={[global.p,{color:'white'}]} onPress={() => handleEdit(selectedEvent.id)}>Submit</Text>
-  </TouchableOpacity>
-  
-             
+              <TouchableOpacity style={[styles.cancelButton]} onPress={() => setModal2Visible(false)}>
+
+                <Text style={[global.p, { color: 'white' }]}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.submitButton]} >
+                <Text style={[global.p, { color: 'white' }]} onPress={() => handleEdit(selectedEvent.id)}>Submit</Text>
+              </TouchableOpacity>
+
+
             </View>
-            <TouchableOpacity style={[styles.closeButton]} onPress={() => setModalVisible(false)}>
-    <Text style={global.p}>X</Text>
-  </TouchableOpacity>
+            <TouchableOpacity style={[styles.closeButton]} onPress={() => setModal2Visible(false)}>
+              <Text style={global.p}>X</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </Modal>
@@ -98,7 +121,7 @@ const agendaPRef = database.collection('users').doc(uid).collection('agendaP')
           setSelectedEvent(filteredEvents[0]);
           setModalVisible(true);
         }}>
-          <Text style={[{fontWeight:'bold'},global.p]}>
+          <Text style={[{ fontWeight: 'bold' }, global.p]}>
             {filteredEvents.map((event, index, array) => {
               const separator = index === array.length - 1 ? '' : '\n\n';
               return `${event.titulo}${separator}`;
@@ -128,6 +151,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 22,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  }, submitButton: {
+    backgroundColor: '#337AB7',
+    padding: 5,
+    width: 100,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  }, cancelButton: {
+    backgroundColor: '#C7254E',
+    padding: 5,
+    width: 100,
+    height: 50,
+    marginRight: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   modalView: {
     margin: 20,
@@ -136,38 +177,45 @@ const styles = StyleSheet.create({
     padding: 35,
     elevation: 5,
   },
-  editButton:{
-    backgroundColor:'#337AB7',
-    padding:5,
-    width:100,
-    height:50,
-    borderRadius:8,
-    alignItems:'center',
-    justifyContent:'center'
-    
+  editButton: {
+    backgroundColor: '#337AB7',
+    padding: 5,
+    width: 100,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
 
-  },deleteButton:{
-    backgroundColor:'#C7254E',
-    padding:5,
-    width:100,
-    height:50,
-    borderRadius:8,
-    alignItems:'center',
-    justifyContent:'center'
+
+  }, deleteButton: {
+    backgroundColor: '#C7254E',
+    padding: 5,
+    width: 100,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
 
   },
   closeButton: {
     position: 'absolute',
     top: 10,
     right: 10,
-    
+
     width: 25,
     height: 25,
     borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
+  }, input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    padding: 8,
+    marginVertical: 20,
+    fontSize: 16,
   },
- 
+
 });
 
 export default EventList;
--- screens/agenda.js
+++ screens/agenda.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect,useCallback,useMemo } from 'react';
-import { Text, View, Modal, TouchableOpacity,StyleSheet,ScrollView,Animated,TextInput } from 'react-native';
-import { Calendar,LocaleConfig,CalendarList } from 'react-native-calendars';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { Text, View, Modal, TouchableOpacity, StyleSheet, ScrollView, Animated, TextInput } from 'react-native';
+import { Calendar, LocaleConfig, CalendarList } from 'react-native-calendars';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Header from '../components/Header';
 import { global } from "../styles/globals";
@@ -14,6 +14,7 @@ import NetInfo from '@react-native-community/netinfo';
 const AgendaScreen = React.memo(() => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModal2Visible, setIsModal2Visible] = useState(false);
   const uid = auth.currentUser.uid
   const [turma, setTurma] = useState('');
   const agendaRef = database.collection('agenda');
@@ -21,35 +22,35 @@ const AgendaScreen = React.memo(() => {
   const [eventPDates, setPEventDates] = useState([]);
   const eventosRef = database.collection('eventos');
   const [agenda, setAgenda] = useState([]);
-  const [newEvent,setNewEvent] = useState('');
+  const [newEvent, setNewEvent] = useState('');
   const today = new Date().toISOString().split('T')[0]; // get today's date in ISO format
   const modalPosition = React.useRef(new Animated.Value(0)).current;
   const agendaPRef = database.collection('users').doc(uid).collection('agendaP')
   const [isConnected, setIsConnected] = useState();
-    const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false)
   function formatDate(timestamp) {
     const date = new Date(timestamp.seconds * 1000);
     const year = date.getFullYear().toString();
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');     
+    const day = date.getDate().toString().padStart(2, '0');
     return `${year}-${month}-${day}`;
   }
-const noEventsMessage = selectedDate === today
-  ? 'Não tens nada agendado para hoje'
-  : 'Não tens nada agendado para este dia';
+  const noEventsMessage = selectedDate === today
+    ? 'Não tens nada agendado para hoje'
+    : 'Não tens nada agendado para este dia';
   const noEventsMessage2 = selectedDate === today
-  ? 'Não tens nada pessoal agendado para hoje'
-  : 'Não tens nada pessoal agendado para este dia';
+    ? 'Não tens nada pessoal agendado para hoje'
+    : 'Não tens nada pessoal agendado para este dia';
   async function loadDataFromStorage() {
     const agendaData = await AsyncStorage.getItem('agenda');
     const turmaData = await AsyncStorage.getItem('turma');
     const agendaPData = await AsyncStorage.getItem('agendaP');
     setTurma(turmaData);
-    console.log(agendaPData)
+
     if (agendaData !== null) {
       const parsedData = JSON.parse(agendaData).map((event) => {
-        const [year,month,day] = event.data.split('-');
-        const localDate = new Date(year, month - 1,day ,0,0,0);
+        const [year, month, day] = event.data.split('-');
+        const localDate = new Date(year, month - 1, day, 0, 0, 0);
         const offset = localDate.getTimezoneOffset() * 60000; // convert minutes to milliseconds
         const utcDate = new Date(localDate.getTime() - offset);
         return {
@@ -58,23 +59,23 @@ const noEventsMessage = selectedDate === today
         };
       });
       setEventDates(parsedData);
-      
-      
+
+
     }
-  
+
     if (agendaPData !== null) {
       const parsedPData = JSON.parse(agendaPData);
-      console.log(agendaPData)
+
       setPEventDates(parsedPData);
-      
+
     }
-   
+
   }
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener((state) => {
       setIsConnected(state.isConnected);
     });
-  
+
     return () => {
       unsubscribe();
     };
@@ -83,66 +84,67 @@ const noEventsMessage = selectedDate === today
     const unsubscribeNetInfo = NetInfo.addEventListener((state) => {
       console.log(isConnected);
       setIsConnected(state.isConnected);
-      
+
     });
     loadDataFromStorage();
-    
+
     const today = new Date();
     setSelectedDate(today.toISOString().split('T')[0]);
-    
-    if (isConnected ) {
-      
-    if (turma) {
-      agendaRef.doc(turma).collection('agenda')
-        .onSnapshot((agendaDoc) => {
-          const agenda = []
-          console.log("mudou")
-          agendaDoc.forEach((document) => {
-            const titulo = document.data().titulo;
-            const data = document.data().data;
-            console.log(titulo)
-            agenda.push({ data: formatDate(data), titulo });
-          });
-          setEventDates(agenda);
-          
-          if (agenda.length > 0) {
-             AsyncStorage.setItem('agenda', JSON.stringify(agenda));
-             
-          }
-          
-        })
-      }
-      agendaPRef
+
+    if (isConnected) {
+
+      if (turma) {
+        agendaRef.doc(turma).collection('agenda')
           .onSnapshot((agendaDoc) => {
-            const agendaP = []
+            const agenda = []
+            console.log("mudou")
             agendaDoc.forEach((document) => {
               const titulo = document.data().titulo;
               const data = document.data().data;
-              const id = document.data().id;
-              agendaP.push({ data: data, titulo, id });
+              console.log(titulo)
+              agenda.push({ data: formatDate(data), titulo });
             });
-            setPEventDates(agendaP);
-            
-            if (agendaP.length > 0) {
-              AsyncStorage.setItem('agendaP', JSON.stringify(agendaP));
-              
+            setEventDates(agenda);
+
+            if (agenda.length > 0) {
+              AsyncStorage.setItem('agenda', JSON.stringify(agenda));
+
             }
-            
+
           })
-        }
-        return () => {
-          unsubscribeNetInfo();
-        };
+      }
+      agendaPRef
+        .onSnapshot((agendaDoc) => {
+          const agendaP = []
+          agendaDoc.forEach((document) => {
+            const titulo = document.data().titulo;
+            const data = document.data().data;
+            const id = document.data().id;
+            agendaP.push({ data: data, titulo, id });
+          });
+          setPEventDates(agendaP);
+
+          if (agendaP.length > 0) {
+            AsyncStorage.setItem('agendaP', JSON.stringify(agendaP));
+
+          }
+
+        })
+    }
+    return () => {
+      unsubscribeNetInfo();
+    };
   }, [isConnected]);
 
-  
+
 
   const onDayPress = useCallback((day) => {
     setSelectedDate(day.dateString);
-    
+    console.log(selectedDate)
+
   }, []);
 
- 
+
   LocaleConfig.locales['pt'] = {
     monthNames: [
       'Janeiro',
@@ -161,7 +163,7 @@ const noEventsMessage = selectedDate === today
     dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
     dayNames: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
     today: 'Hoje',
-    
+
   };
   const openModal = () => {
     setIsModalVisible(true);
@@ -170,36 +172,63 @@ const noEventsMessage = selectedDate === today
   const closeModal = () => {
     setIsModalVisible(false);
   };
-  
-  const submitEvent =  () => {
-    const eventPRef = agendaPRef.doc(); // create a new document reference with a random ID
-    const eventPData = {
-      titulo: newEvent,
-      data: selectedDate,
-      id: eventPRef.id // add the ID to the data object
-    };
-     eventPRef.set(eventPData); // add the data to the document
+
+  const submitEvent = async () => {
+    const today = new Date().setHours(0, 0, 0, 0);
+    const selectedDateObj = new Date(selectedDate);
+    selectedDateObj.setHours(0, 0, 0, 0);
+
+    if (selectedDateObj.getTime() >= today) {
+      const eventP = [];
+      const eventPRef = agendaPRef.doc();
+      const eventPData = {
+        titulo: newEvent,
+        data: selectedDate,
+        id: eventPRef.id
+      };
+      eventP.push(eventPData);
+      setPEventDates(eventP);
+      eventPRef.set(eventPData);
+      AsyncStorage.setItem('agendaP', JSON.stringify(eventP));
+      const agendaPData = await AsyncStorage.getItem('agendaP');
+    } else {
+      setIsModal2Visible(true)
+    }
   }
 
 
-LocaleConfig.defaultLocale = 'pt'
-const markedDates = useMemo(() => {
-  const allDates = [...eventDates, ...eventPDates];
-  return allDates.reduce((obj, date) => {
-    obj[date.data] = {
-      selected: true,
-      title: date.titulo,
-    };
-    return obj;
-  }, {});
-}, [eventDates, eventPDates]);
-const currentDate = moment().format('YYYY-MM-DD');
-const currentYear = moment().format('YYYY');
-const lastYear = moment().subtract(1, 'year').format('YYYY');
 
-const minDate = `${lastYear}-01-01`;
-const maxDate = `${currentYear}-12-31`;
-const disabledMonths = {};
+  const markedDates = useMemo(() => {
+    const allDates = [...eventDates, ...eventPDates];
+    const markedDatesObj = allDates.reduce((obj, date) => {
+      obj[date.data] = {
+        selected: true,
+        title: date.titulo,
+        color: 'blue',
+      };
+      return obj;
+    }, {});
+
+    if (selectedDate) {
+      const existingMarkedDateObj = markedDatesObj[selectedDate];
+      markedDatesObj[selectedDate] = {
+        ...existingMarkedDateObj,
+
+        marked: true,
+      };
+    }
+
+    return markedDatesObj;
+  }, [eventDates, eventPDates, selectedDate]);
+
+
+  const currentDate = moment().format('YYYY-MM-DD');
+  const currentYear = moment().format('YYYY');
+  const lastYear = moment().subtract(1, 'year').format('YYYY');
+
+  const minDate = `${lastYear}-01-01`;
+  const maxDate = `${currentYear}-12-31`;
+  const disabledMonths = {};
 
   const momentMinDate = moment(minDate);
   const momentMaxDate = moment(maxDate);
@@ -218,80 +247,104 @@ const disabledMonths = {};
 
 
   return (
-    
+
     <>
-     <Modal
-  animationType='fade'
-  visible={isModalVisible}
-  onRequestClose={() => setIsModalVisible(false)}
-  transparent={true}
->
-  <View style={styles.modalContainer}>
-    <View style={styles.modalContent}>
-    <TextInput
-      style={styles.input}
-      placeholder="Titulo do evento"
-      onChangeText={text => setNewEvent(text)}
-      value={newEvent}
-    />
-      <TouchableOpacity style={styles.submitButton} onPress={() => {
-  submitEvent();
-  closeModal();
-}}>
-      <Text style={styles.submitButtonText} >Save</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={closeModal}>
-      <Text style={styles.cancelButtonText}>Cancel</Text>
-    </TouchableOpacity>
-    </View>
-  </View>
-</Modal>
+      <Modal
+        animationType='fade'
+        visible={isModalVisible}
+        onRequestClose={() => setIsModalVisible(false)}
+        transparent={true}
+      >
+        <View style={styles.modalContainer}>
 
-    <Header></Header>
-    <View style={{ flex: 1 }}>
-    <Calendar
-        onDayPress={onDayPress}
-        markedDates={markedDates}
-        pagingEnabled={true}
-        hideExtraDays={true}
-        firstDay={1}
-        minDate={minDate}
-        maxDate={maxDate}
-        theme={{
-          backgroundColor:'rgb(242,242,242)',
-          calendarBackground:'rgb(242,242,242)',
-          textMonthFontFamily:'sans-bold',
-          textMonthFontSize:36,
-          monthTextColor:'#1E1E1E',
+          <View style={styles.modalContent}>
+            <TouchableOpacity style={[styles.closeButton]} onPress={closeModal}>
+              <Text style={global.p}>X</Text>
+            </TouchableOpacity>
+            <Text style={global.h2}>Criar evento</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Titulo do evento"
+              onChangeText={text => setNewEvent(text)}
+              value={newEvent}
+            /><View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+              <TouchableOpacity style={styles.cancelButton} onPress={closeModal}>
+                <Text style={[global.p, { color: 'white' }]}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.submitButton} onPress={() => {
+                submitEvent();
+                closeModal();
+              }}>
 
-          
-        }}
-      />
-      
-      
-      <EventList
-  events={eventDates}
-  selectedDate={selectedDate}
-  noEventsMessage={noEventsMessage}
-/>
-<EventPList
-  events={eventPDates}
-  selectedDate={selectedDate}
-  noEventsMessage={noEventsMessage2}
-  />
+                <Text style={[global.p, { color: 'white' }]} >Save</Text>
+              </TouchableOpacity>
 
-  
-      <View style={styles.addEvent}>
-      <TouchableOpacity style={styles.circleButton} onPress={openModal}>
-    <Text style={styles.plus}>+</Text>
-  </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        animationType='fade'
+        visible={isModal2Visible}
+        onRequestClose={() => setIsModal2Visible(false)}
+        transparent={true}
+      >
+        <View style={styles.modalContainer}>
+
+          <View style={styles.modalContent}>
+            <TouchableOpacity style={[styles.closeButton]} onPress={() => setIsModal2Visible(false)}>
+              <Text style={global.p}>X</Text>
+            </TouchableOpacity>
+            <Text style={global.p}>Não podes agendar nada para antes do dia de hoje</Text>
+          </View>
+        </View>
+      </Modal>
+
+      <Header></Header>
+      <View style={{ flex: 1 }}>
+        <Calendar
+          onDayPress={onDayPress}
+          markedDates={markedDates}
+          pagingEnabled={true}
+          hideExtraDays={true}
+          firstDay={1}
+          minDate={minDate}
+          maxDate={maxDate}
+          theme={{
+            backgroundColor: 'rgb(242,242,242)',
+            calendarBackground: 'rgb(242,242,242)',
+            textMonthFontFamily: 'sans-bold',
+            textMonthFontSize: 36,
+            monthTextColor: '#1E1E1E',
+
+
+          }}
+        />
+
+        <Text style={[global.h2, { marginLeft: 20, marginTop: 20, color: 'rgba(30, 30, 30, 0.63)' }]}>{selectedDate}</Text>
+        <EventList
+          events={eventDates}
+          selectedDate={selectedDate}
+          noEventsMessage={noEventsMessage}
+        />
+        <EventPList
+          events={eventPDates}
+          selectedDate={selectedDate}
+          noEventsMessage={noEventsMessage2}
+        />
+
+
+        <View style={styles.addEvent}>
+          <TouchableOpacity style={styles.circleButton} onPress={openModal}>
+            <Text style={styles.plus}>+</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
     </>
   );
 });
 const styles = StyleSheet.create({
-  
+
   addEvent: {
     position: 'absolute',
     bottom: 16,
@@ -308,7 +361,7 @@ const styles = StyleSheet.create({
   plus: {
     color: 'white',
     fontSize: 24,
-    
+
   },
   modalContainer: {
     flex: 1,
@@ -317,11 +370,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalContent: {
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    padding: 20,
-    width: '80%',
-    alignItems: 'center',
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 35,
+    elevation: 5,
+
   },
   input: {
     borderWidth: 1,
@@ -329,25 +383,44 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     padding: 8,
     marginVertical: 20,
-    width: '100%',
     fontSize: 16,
   },
   submitButton: {
-    backgroundColor: '#3498db',
-    justifyContent:'center',
-    alignItems:'center',
-    paddingVertical: 5,
-    width:70,
-    paddingHorizontal: 10,
-    borderRadius: 4,
+    backgroundColor: '#337AB7',
+    padding: 5,
+    width: 100,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
   submitButtonText: {
     color: '#fff',
     fontSize: 16,
   },
-  cancelButtonText:{
-    marginTop:5
-  }
-  
+  cancelButton: {
+    backgroundColor: '#C7254E',
+    padding: 5,
+    width: 100,
+    height: 50,
+    marginRight: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  cancelButtonText: {
+    color: 'white'
+  }, closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    width: 25,
+    height: 25,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
 });
 export default AgendaScreen;  
